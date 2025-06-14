5d7ebc42a9bcb454cdd2c51035505d1339dfc2b5
fitronansori <fitronansori7@gmail.com>
2025-06-14 13:45:58 +0800

    refactor: update Logo component for responsive display

--- src/components/layouts/root/Header/index.tsx
+++ src/components/layouts/root/Header/index.tsx
@@ -12,7 +12,10 @@ const Header = () => {
     <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-4">
-          <Logo />
+          <Logo className="hidden lg:block" />
+
+          <Logo className="lg:hidden" show_text={false} />
+
           <NavLinks className="hidden lg:flex" />
         </div>
 
