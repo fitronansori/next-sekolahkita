830aeb1de6296c3080dcb7704c065b8c27cfc799
fitronansori <fitronansori7@gmail.com>
2025-06-14 17:31:32 +0800

    feat: add Features component to Home for enhanced content display

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import Features from '@/components/layouts/root/Features';
 import Hero from '@/components/layouts/root/Hero';
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Features />
     </>
   );
 };
